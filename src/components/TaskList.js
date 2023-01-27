3b947fbfc20814b045ed78dfc71b09f235729922
annadu <duyuan0921@gmail.com>
2023-01-26 21:30:18 -0800

    Wave 1 change function getTaskListJSX

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
-      return (
-        <Task
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          isComplete={task.isComplete}
+const TaskList = ({tasks}) => {
+  
+    const taskComponent = tasks.map((task) => {
+        return (
+      <Task
+        key={task.id}
+        id={task.id}
+        title={task.title}
+        isComplete={task.isComplete}
         />
       );
     });
+    return (<ul className="tasks__list no-bullet">{taskComponent}</ul>);
   };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
-};
+  
+
 
 TaskList.propTypes = {
   tasks: PropTypes.arrayOf(
